5b74c5d20e203a000bf6c4602f634848070bdba2
zooof2017 <zooof2017@yandex.ru>
2018-03-08 13:49:18 +0300

    first raw

---
+++ config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Этот токен невалидный, можете даже не пробовать :)
+token = '550466591:AAEVjNkM6AEOGajMGlnaGsu8hqUyLL_AzOE'
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
+
